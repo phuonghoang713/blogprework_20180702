2f7fe08a0d14aa8b88aee051c12ae9e5321ab071
phuonghoang713 <phuonghoang713@gmail.com>
2018-06-24 15:40:40 +0700

    finish index

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20180624021642) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
