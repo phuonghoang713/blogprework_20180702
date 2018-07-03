164268c6d97741440669be6b1f8ff07721c2253e
phuonghoang713 <phuonghoang713@gmail.com>
2018-07-04 06:55:27 +0700

    create item and correct flow data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,28 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+  
+  end
+
+  def create
+    @post = Post.new(params_post)
+
+    if @post.save
+      redirect_to posts_path
+    else 
+      redirect_to posts_path
+    end
+
+  end
+  
+  private 
+  def params_post 
+    params.require(:post).permit(:body, :title)
+  end 
 end
