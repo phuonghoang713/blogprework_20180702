9b5d08cf98d5cd294aa883c85c38d3079872b251
phuonghoang713 <phuonghoang713@gmail.com>
2018-07-04 17:54:16 +0700

    edit and delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])#because it is a string
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     if @comment.save
       redirect_to @post
@@ -10,8 +10,28 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to @post
+    else
+      redirect_to post
+    end
   end
 
+  def edit 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to @post
+    else 
+      redirect_to @post
+    end
+  end
   def comment_params
     params.require(:comment).permit(:body)
   end
